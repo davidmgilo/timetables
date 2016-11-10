1d16c0f767a70153fff3dff62735df8fada5eddd
David Martinez <davidmgilo@gmail.com>
2016-11-10 21:42:38 +0100

    added fields torn & name to shifts

--- database/migrations/2016_11_10_203631_create_shifts_table.php
+++ database/migrations/2016_11_10_203631_create_shifts_table.php
@@ -15,6 +15,8 @@ class CreateShiftsTable extends Migration
     {
         Schema::create('shifts', function (Blueprint $table) {
             $table->increments('id');
+            $table->enum('torn',['tarda','mati']);
+            $table->string('name');
             $table->timestamps();
         });
     }
