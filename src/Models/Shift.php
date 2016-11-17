e87535f8f7525faf4fd7925a446ee3ccafbf3037
David Martinez <davidmgilo@gmail.com>
2016-11-17 21:45:59 +0100

    fillables in Shift Model

--- src/Models/Shift.php
+++ src/Models/Shift.php
@@ -4,12 +4,21 @@ namespace Scool\Timetables\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Shift
+ * @package Scool\Timetables\Models
+ */
 class Shift extends Model
 {
-    //fillable
 
+    protected $fillable = [
+        'torn', 'name',
+    ];
 
     //TODO falta la relació completa, falta la classe Timeslot. Traits
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function timeslots()
     {
         return $this->belongsToMany(App\Timeslot::class);
