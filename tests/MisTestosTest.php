ab8890b72d0d605ef0ee7940a3da9d8ce2911681
David Martinez <davidmgilo@gmail.com>
2016-11-22 16:26:37 +0100

    @test

--- tests/MisTestosTest.php
+++ tests/MisTestosTest.php
@@ -7,9 +7,11 @@
 class MisTestosTest extends PHPUnit_Framework_TestCase
 {
     /**
-     *
+     * Test to know if there are shifts
+     * @test
+     * @group failing
      */
-    public function testprovatonta(){
+    public function it_shows_shifts(){
       echo "adaadad";
     }
 
