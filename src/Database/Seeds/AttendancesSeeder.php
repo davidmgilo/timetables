fe3ed899617b9f3810a005b888f69383f82b4d28
David Martinez <davidmgilo@gmail.com>
2016-12-15 20:14:31 +0100

    seeder attendances

---
+++ src/Database/Seeds/AttendancesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Scool\Timetables\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class AttendancesSeeder
+ * @package Scool\Timetables\Database\Seeds
+ */
+class AttendancesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
