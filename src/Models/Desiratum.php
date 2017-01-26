531456a3215e595c183f0b7f38c55cddf6dcbece
David Martinez <davidmgilo@gmail.com>
2017-01-26 20:17:23 +0100

    desiratum model as before

--- src/Models/Desiratum.php
+++ src/Models/Desiratum.php
@@ -2,14 +2,27 @@
 
 namespace Scool\Timetables\Models;
 
+use Acacha\Names\Traits\Nameable;
+use Acacha\Stateful\Traits\StatefulTrait;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
 class Desiratum extends Model implements Transformable
 {
-    use TransformableTrait;
+    use TransformableTrait, Nameable, StatefulTrait;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'torn', 'name',
+    ];
+
+    //TODO falta la relació completa, falta la classe Timeslot. Traits
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function timeslots()
+    {
+        return $this->belongsToMany(App\Timeslot::class);
+    }
 
 }
