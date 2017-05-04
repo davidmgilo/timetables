6a385193e221c4d96709ead6bbf0efb89e1151fe
David Martinez <davidmgilo@gmail.com>
2017-05-04 21:19:57 +0200

    classrooms

--- src/Models/Classroom.php
+++ src/Models/Classroom.php
@@ -4,7 +4,24 @@ namespace Scool\Timetables\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Classroom
+ * @package Scool\Timetables\Models
+ */
 class Classroom extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function lessons()
+    {
+        return $this->hasMany(\Scool\Timetables\Models\Lesson::class);
+    }
 }
--- src/Models/Lesson.php
+++ src/Models/Lesson.php
@@ -8,37 +8,59 @@ use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
+/**
+ * Class Lesson
+ * @package Scool\Timetables\Models
+ */
 class Lesson extends Model implements Transformable, Stateful
 {
     use TransformableTrait, StatefulTrait;
 
     protected $fillable = ['id','location_id','day_id','timeslot_id','classroom_id'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function locations()
     {
         return $this->belongsTo(\Scool\Timetables\Models\Location::class); //TODO
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function days()
     {
         return $this->belongsTo(\Scool\Timetables\Models\Day::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function timeslots()
     {
         return $this->belongsTo(\Scool\Timetables\Models\Timeslot::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function classrooms()
     {
         return $this->belongsTo(\Scool\Timetables\Models\Classroom::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function users()
     {
         return $this->belongsToMany(\App\User::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function submodules()
     {
         return $this->belongsToMany(\Submodule::class); //TODO
