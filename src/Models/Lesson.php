c3bce100e05c6f0b245036b5d7c686c0b1ac9f04
David Martinez <davidmgilo@gmail.com>
2017-05-04 20:54:59 +0200

    updated model lesson

--- src/Models/Lesson.php
+++ src/Models/Lesson.php
@@ -2,14 +2,45 @@
 
 namespace Scool\Timetables\Models;
 
+use Acacha\Stateful\Contracts\Stateful;
+use Acacha\Stateful\Traits\StatefulTrait;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Lesson extends Model implements Transformable
+class Lesson extends Model implements Transformable, Stateful
 {
-    use TransformableTrait;
+    use TransformableTrait, StatefulTrait;
 
-    protected $fillable = [];
+    protected $fillable = ['id','location_id','day_id','timeslot_id','classroom_id'];
 
+    public function locations()
+    {
+        return $this->belongsTo(\Scool\Timetables\Models\Location::class); //TODO
+    }
+
+    public function days()
+    {
+        return $this->belongsTo(\Scool\Timetables\Models\Day::class);
+    }
+
+    public function timeslots()
+    {
+        return $this->belongsTo(\Scool\Timetables\Models\Timeslot::class);
+    }
+
+    public function classrooms()
+    {
+        return $this->belongsTo(\Scool\Timetables\Models\Classroom::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(\App\User::class);
+    }
+
+    public function submodules()
+    {
+        return $this->belongsToMany(\Submodule::class); //TODO
+    }
 }
